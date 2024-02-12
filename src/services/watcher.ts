86bf42d894b067be3cafdfcba0caeb263f96fbee
Maaz Hassan <h.maaz809@hotmail.com>
2024-02-12 07:51:00 -0700

    feat: initialize watcher for files in POSTS_PATH

---
+++ src/services/watcher.ts
@@ -0,0 +1,39 @@
+import Watcher from 'watcher';
+import matter from 'gray-matter';
+import fs from 'fs';
+import sequelize from './db.js';
+import { Post } from '../models/post.model.js';
+import { Path } from 'typescript';
+
+export default function watch() {
+  const watcher = new Watcher(process.env.POSTS_PATH, {
+    renameDetection: true,
+    persistent: true
+  });
+
+  watcher.on('change', path => {
+    console.log(`File ${path} has been changed`);
+
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      // console.log(data);
+      const { content: content, data: frontMatter } = matter(data);
+      console.log(frontMatter);
+      console.log(content);
+      
+      if (frontMatter.draft === 'true') {
+        return;
+      }
+
+
+
+    });
+  });
+
+  watcher.on('rename', (oldPath, newPath) => {
+    console.log(`File ${oldPath} has been renamed to ${newPath}`);
+  });
+}
